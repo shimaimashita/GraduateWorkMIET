107dc37408805e7d410e4e7baad92f6839a8d6e0
shimaimashita <kichuslyaev@gmail.com>
2022-05-15 21:19:58 +0300

    local library

--- askstudents/asking/admin.py
+++ askstudents/asking/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, BookInstance, Genre, Language
+
+class BookInline(admin.StackedInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name',
+            ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+
+
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+            (None, {
+                'fields': ('book', 'imprint', 'id')
+            }),
+            ('Availability', {
+                'fields': ('status', 'due_back')
+            }),
+    )
+
+class BookInstanceInline(admin.StackedInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
--- askstudents/askstudents/urls.py
+++ askstudents/askstudents/urls.py
@@ -32,3 +32,7 @@ urlpatterns += [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('uploader/', include('uploader.urls')),
+]
